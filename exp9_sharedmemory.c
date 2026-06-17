f23abcc868be5433d7c208c73f1388d868aa2b09
lakshnasm <lakshnasm098@gmail.com>
2026-06-17 15:17:54 +0530

    Update exp9_sharedmemory.c

--- exp9_sharedmemory.c
+++ exp9_sharedmemory.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+#define SHM_SIZE 1024
+
 int main()
 {
-    key_t key = ftok("shmfile",65);
+    key_t key = ftok("shmfile", 65);
 
-    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    int shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666);
 
-    char *str = (char*)shmat(shmid,NULL,0);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
 
-    strcpy(str,"Hello Shared Memory");
+    char *shm_ptr = (char*)shmat(shmid, NULL, 0);
 
-    printf("Data written in memory: %s\n",str);
+    if (shm_ptr == (char*)(-1))
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
 
-    shmdt(str);
-    
-    shmctl(shmid,IPC_RMID,NULL);
+    strcpy(shm_ptr, "Hello, shared memory!");
+
+    printf("Data written to shared memory: %s\n", shm_ptr);
+
+    shmdt(shm_ptr);
+
+    shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
 }
