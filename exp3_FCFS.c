0a4990ede9393fb56721b016146eeb46dd208f3d
lakshnasm <lakshnasm098@gmail.com>
2026-06-20 12:29:16 +0530

    Implement file I/O for process scheduling
    Added file handling to read and write process data.

--- exp3_FCFS.c
+++ exp3_FCFS.c
@@ -1,75 +1,22 @@
-#include <stdio.h>
 
-int main()
-{
-    int A[100][4];
-    int i, j, n, total = 0, index, temp;
-    float avg_wt, avg_tat;
-
-    printf("Enter number of process: ");
-    scanf("%d", &n);
-
-    printf("Enter Burst Time:\n");
-
-    for(i = 0; i < n; i++)
-    {
-        printf("P%d: ", i + 1);
-        scanf("%d", &A[i][1]);
-        A[i][0] = i + 1;
+#include <stdio.h> 
+#include <stdlib.h>
+int main() {
+    FILE *fptr1, *fptr2; char filename[100], c;
+    printf("Enter the filename to open for reading \n"); scanf("%s", filename);
+    fptr1 = fopen(filename, "r");
+    if(fptr1 == NULL){
+        printf("Cannot open file %s \n", filename); exit(0);
     }
-
-    for(i = 0; i < n; i++)
-    {
-        index = i;
-
-        for(j = i + 1; j < n; j++)
-        {
-            if(A[j][1] < A[index][1])
-                index = j;
-        }
-
-        temp = A[i][1];
-        A[i][1] = A[index][1];
-        A[index][1] = temp;
-
-        temp = A[i][0];
-        A[i][0] = A[index][0];
-        A[index][0] = temp;
+    printf("Enter the filename to open for writing \n"); scanf("%s", filename);
+    fptr2 = fopen(filename, "w");
+    if(fptr2 == NULL){
+        printf("Cannot open file %s \n", filename); exit(0);
     }
-
-    A[0][2] = 0;
-
-    for(i = 1; i < n; i++)
-    {
-        A[i][2] = 0;
-
-        for(j = 0; j < i; j++)
-            A[i][2] += A[j][1];
-
-        total += A[i][2];
-    }
-
-    avg_wt = (float)total / n;
-    total = 0;
-
-    printf("P\tBT\tWT\tTAT\n");
-
-    for(i = 0; i < n; i++)
-    {
-        A[i][3] = A[i][1] + A[i][2];
-        total += A[i][3];
-
-        printf("P%d\t%d\t%d\t%d\n",
-               A[i][0],
-               A[i][1],
-               A[i][2],
-               A[i][3]);
-    }
-
-    avg_tat = (float)total / n;
-
-    printf("Average Waiting Time = %f\n", avg_wt);
-    printf("Average Turnaround Time = %f\n", avg_tat);
-
+    c = fgetc(fptr1); while (c != EOF) {
+        fputc(c, fptr2); 
+        c = fgetc(fptr1); }
+    printf("\nContents copied to %s", filename);
+    fclose(fptr1); fclose(fptr2); 
     return 0;
 }
